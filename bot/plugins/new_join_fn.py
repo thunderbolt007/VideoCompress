64bc4f9e0f4ad07c40d10631b9a56b3d9398e866
thunderbolt007 <58079201+thunderbolt007@users.noreply.github.com>
2021-01-06 22:08:31 +0530

    new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -29,10 +29,10 @@ async def help_message_f(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
+                    InlineKeyboardButton('MASTER', url='https://t.me/Accidental_Influencer')
                 ],
                 [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
+                    InlineKeyboardButton('MASTER', url='https://t.me/Accidental_Influencer')
                 ]
             ]
         ),
